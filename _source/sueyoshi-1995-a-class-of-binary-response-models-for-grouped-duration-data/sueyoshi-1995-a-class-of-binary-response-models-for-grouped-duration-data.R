0d58f111de8be0b77b9ba58a2dbd51b9d38f0551
unknown <tirthankar.chakravarty@gmail.com>
2016-01-15 16:57:04 -0500

    Testing pushing to the blog using the "brocks" package (http://brendanrocks.com/blogging-with-rmarkdown-knitr-jekyll/).

---
+++ _source/sueyoshi-1995-a-class-of-binary-response-models-for-grouped-duration-data/sueyoshi-1995-a-class-of-binary-response-models-for-grouped-duration-data.R
@@ -0,0 +1,4 @@
+# This R file accomanies the .Rmd blog post
+# _source/sueyoshi-1995-a-class-of-binary-response-models-for-grouped-duration-data/2016-01-15-sueyoshi-1995-a-class-of-binary-response-models-for-grouped-duration-data.Rmd
+
+
